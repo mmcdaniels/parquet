67f9f9fd7d5b969539e0d9ec7f4f40018199e88a
mmcdaniels <64846025+mmcdaniels@users.noreply.github.com>
2024-10-06 17:50:18 -0700

    `mix new parquet`

---
+++ lib/parquet.ex
@@ -0,0 +1,18 @@
+defmodule Parquet do
+  @moduledoc """
+  Documentation for `Parquet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Parquet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parquet_test.exs
@@ -0,0 +1,8 @@
+defmodule ParquetTest do
+  use ExUnit.Case
+  doctest Parquet
+
+  test "greets the world" do
+    assert Parquet.hello() == :world
+  end
+end
