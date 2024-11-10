a5dfea99e253298fbeee0a69f3aaab93e9d1718d
mmcdaniels <64846025+mmcdaniels@users.noreply.github.com>
2024-11-09 16:00:00 -0800

    Add binary test helpers

---
+++ test/support/test_combinators_binary.ex
@@ -0,0 +1,34 @@
+defmodule Combinators.BinaryTest.Helpers do
+  use ExUnitProperties
+  alias Combinators.Result
+
+  @doc """
+  Calculates the cursor position assuming that `bitstr` is parsed successfully.
+  """
+  def calculate_binary_cursor(bitstr) do
+    position =
+      for <<_::size(1) <- bitstr>>, reduce: 1 do
+        c -> c + 1
+      end
+
+    %Result.Binary.Cursor{position: position}
+  end
+
+  @doc """
+  Generates a bit.
+  """
+  def bit_gen do
+    gen all(bit <- StreamData.bitstring(length: 1)) do
+      bit
+    end
+  end
+
+  @doc """
+  Generates a bitstring.
+  """
+  def bitstr_gen do
+    gen all(bitstr <- StreamData.bitstring()) do
+      bitstr
+    end
+  end
+end
