6dd7c42413c4a014365e3d94840a290f7e2e4143
mmcdaniels <64846025+mmcdaniels@users.noreply.github.com>
2024-11-09 16:00:00 -0800

    Implement tagged_sequence/1 combinator

--- lib/combinators/control.ex
+++ lib/combinators/control.ex
@@ -24,4 +24,49 @@ defmodule Combinators.Control do
       end
     end
   end
+
+  def tagged_sequence(tagged_parsers, parsed_map \\ %{}) do
+    fn input, cursor ->
+      case tagged_parsers do
+        [] ->
+          {:ok,
+           %ParseResult.Ok{
+             parsed: parsed_map,
+             remaining: input,
+             cursor: cursor
+           }}
+
+        [tagged_parser | rest_tagged_parsers] ->
+          {tag, parser} = tagged_parser
+
+          if not is_atom(tag) do
+            {
+              :error,
+              :tag_not_an_atom,
+              "Tag `#{tag}` should be an atom or nil"
+            }
+          else
+            with {:ok,
+                  %ParseResult.Ok{
+                    parsed: parsed,
+                    remaining: remaining,
+                    cursor: position
+                  }} <- parser.(input, cursor) do
+              if Map.has_key?(parsed_map, tag) do
+                {
+                  :error,
+                  :duplicate_tag,
+                  "Found duplicated tag `#{tag}`"
+                }
+              else
+                parsed_map =
+                  if is_nil(tag), do: parsed_map, else: Map.put(parsed_map, tag, parsed)
+
+                tagged_sequence(rest_tagged_parsers, parsed_map).(remaining, position)
+              end
+            end
+          end
+      end
+    end
+  end
 end
--- test/combinators/control_test.exs
+++ test/combinators/control_test.exs
@@ -17,6 +17,7 @@ defmodule Combinators.Control.Test do
   import Combinators.Control,
     only: [
       sequence: 1,
+      tagged_sequence: 1
     ]
 
   describe "sequence/1" do
@@ -55,6 +56,97 @@ defmodule Combinators.Control.Test do
 
     test "errors when not enough input" do
       assert sequence([char(), char()]) |> run("a") ==
+               {
+                 :error,
+                 %ParseResult.Error{
+                   code: :no_more_chars,
+                   message: "No more chars @(1L:2C)",
+                   cursor: %ParseResult.Text.Cursor{line: 1, column: 2}
+                 }
+               }
+    end
+  end
+
+  describe "tagged_sequence/1" do
+    property "parses tagged sequence" do
+      ExUnitProperties.check all(
+                               {first, middle, last} <- {chr_gen(), chr_gen(), chr_gen()},
+                               remaining <- str_gen(),
+                               seq = to_string([first, middle, last]),
+                               input = seq <> remaining
+                             ) do
+        assert tagged_sequence([
+                 {:char1, char()},
+                 {:char2, char_is(middle)},
+                 {:char3, char()}
+               ])
+               |> run(input) ==
+                 {
+                   :ok,
+                   %ParseResult.Ok{
+                     parsed: %{
+                       :char1 => first,
+                       :char2 => middle,
+                       :char3 => last
+                     },
+                     remaining: remaining,
+                     cursor: calculate_text_cursor(seq)
+                   }
+                 }
+      end
+    end
+
+    property "ignores nil tags" do
+      ExUnitProperties.check all(
+                               {first, middle, last} <- {chr_gen(), chr_gen(), chr_gen()},
+                               remaining <- str_gen(),
+                               seq = to_string([first, middle, last]),
+                               input = seq <> remaining
+                             ) do
+        assert tagged_sequence([
+                 {:char1, char()},
+                 {nil, char_is(middle)},
+                 {:char3, char()}
+               ])
+               |> run(input) ==
+                 {
+                   :ok,
+                   %ParseResult.Ok{
+                     parsed: %{
+                       :char1 => first,
+                       :char3 => last
+                     },
+                     remaining: remaining,
+                     cursor: calculate_text_cursor(seq)
+                   }
+                 }
+      end
+    end
+
+    test "errors when duplicate tag is found" do
+      assert tagged_sequence([{:char1, char()}, {:char1, char()}, {:char3, char()}]) |> run("abc") ==
+               {
+                 :error,
+                 :duplicate_tag,
+                 "Found duplicated tag `char1`"
+               }
+    end
+
+    test "errors when tag is not an atom" do
+      assert tagged_sequence([{:char1, char()}, {"char2", char()}]) |> run("ab") ==
+               {
+                 :error,
+                 :tag_not_an_atom,
+                 "Tag `char2` should be an atom or nil"
+               }
+    end
+
+    test "errors when not enough input" do
+      assert tagged_sequence([
+               {:char1, char()},
+               {:char2, char()}
+             ])
+             |> run("a") ==
                {
                  :error,
                  %ParseResult.Error{
