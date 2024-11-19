46b9c2ead3baf3d00c3ca9d37f63a20c598810f2
mmcdaniels <64846025+mmcdaniels@users.noreply.github.com>
2024-11-19 08:24:41 -0800

    Add a sample Parquet file and a script to summarize the file contents

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A pure Elixir/OTP library for reading and writing Parquet files.
 
 For now, this is a learning project to understand how Parquet files work by writing an implementation.
+
+## Running
+
+To print data about the included sample file, run:
+```elixir
+mix run -e 'Parquet.summarize_sample()'
+```
--- lib/parquet.ex
+++ lib/parquet.ex
@@ -1,18 +1,56 @@
 defmodule Parquet do
-  @moduledoc """
-  Documentation for `Parquet`.
-  """
+  alias Parquet.Data.Reader
 
-  @doc """
-  Hello world.
+  @separator_width 200
 
-  ## Examples
+  def summarize_sample do
+    summarize("parquets/sample.parquet")
+  end
 
-      iex> Parquet.hello()
-      :world
+  def summarize(path) do
+    # Spawn the Parquet reader process
+    {:ok, reader_pid} = Parquet.Data.Reader.start_link(path)
 
-  """
-  def hello do
-    :world
+    file_metadata = Reader.get_file_metadata(reader_pid)
+    print_summary("FILE METADATA", file_metadata)
+
+    schema_tree = Reader.get_schema_tree(reader_pid)
+    print_summary("SCHEMA TREE", schema_tree)
+
+    column_order = Reader.get_column_order(reader_pid)
+    print_summary("COLUMN ORDER", column_order)
+
+    for column_name <- column_order do
+      row_groups = Reader.fetch_column(reader_pid, column_name)
+      print_summary("FETCH COLUMN - #{column_name}", fn -> print_row_groups(row_groups) end)
+    end
+  end
+
+  defp print_summary(header, thing) do
+    IO.puts("\n")
+    IO.puts(String.duplicate("*", @separator_width))
+    IO.puts(header)
+    IO.puts(String.duplicate("↓", @separator_width))
+
+    if is_function(thing) do
+      thing.()
+    else
+      IO.inspect(thing, pretty: true)
+    end
+
+    IO.puts(String.duplicate("-", @separator_width))
+  end
+
+  defp print_row_groups(row_groups) do
+    Enum.with_index(row_groups, fn rg, rg_index ->
+      Enum.with_index(rg, fn cc, cc_index ->
+        IO.puts("")
+        IO.puts(String.duplicate("*", floor(@separator_width / 4)))
+        IO.puts("Row Group: #{rg_index}")
+        IO.puts("Column Chunk: #{cc_index}")
+        IO.puts(String.duplicate("*", floor(@separator_width / 4)))
+        IO.inspect(cc, pretty: true)
+      end)
+    end)
   end
 end
